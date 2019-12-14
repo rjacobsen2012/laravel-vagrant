e9c73825a3becef43cc4b2715a9336fb2405214b
Richard Jacobsen <rjacobsen2009@gmail.com>
2019-12-14 11:48:44 -0500

    updates to allow customizing the vagrant

--- src/setup/provision.sh
+++ src/setup/provision.sh
@@ -6,6 +6,9 @@ db_user=$3
 db_password=$4
 vagrant_name=$5
 folder_path=$6
+php_version=$7
+phantomjs=$8
+use_chromedriver=$9
 
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
@@ -27,16 +30,32 @@ sudo usermod -a -G www-data vagrant
 
 echo_msg ${GREEN} "Provisioning virtual machine"
 
+php_install_version=php
+
+if [[ "${php_version}" = '7.2' ]]; then
+    php_install_version="7.2"
+fi
+
+if [[ "${php_version}" = '7.1' ]]; then
+    php_install_version="7.1"
+fi
+
 phppresent=`which php`
 if [[ "$phppresent" = "" ]]; then
     echo_msg ${GREEN} "Updating os"
-    sudo apt update -y > /dev/null 2>&1
+    sudo apt-get update -y > /dev/null 2>&1
 
     echo_msg ${GREEN} "Upgrading os"
-    sudo apt upgrade -y > /dev/null 2>&1
+    sudo apt-get upgrade -y > /dev/null 2>&1
 
-    echo_msg ${GREEN} "Installing php 7.2"
-    sudo apt install -y php > /dev/null 2>&1
+    if [[ "${php_version}" = '7.1' ]]; then
+        sudo apt-get install software-properties-common > /dev/null 2>&1
+        sudo add-apt-repository ppa:ondrej/php > /dev/null 2>&1
+        sudo apt-get update -y > /dev/null 2>&1
+    fi
+
+    echo_msg ${GREEN} "Installing php ${php_version}"
+    sudo apt-get install -y php"${php_install_version}" > /dev/null 2>&1
 
     echo_msg ${GREEN} "Installing php packages"
     sudo install -y build-essential tcl libsqlite3-dev ruby-dev > /dev/null 2>&1
@@ -44,19 +63,19 @@ if [[ "$phppresent" = "" ]]; then
     echo postfix postfix/main_mailer_type string 'Internet Site' | sudo debconf-set-selections
     echo "mysql-server-5.7 mysql-server/root_password password $db_password" | sudo debconf-set-selections
     echo "mysql-server-5.7 mysql-server/root_password_again password $db_password" | sudo debconf-set-selections
-    sudo apt install -y php-common php-dev php-cli php-fpm curl php-curl php-gd mcrypt php-mysql php-imap php-mbstring php-zip nginx php-bcmath php-xml chrpath libssl-dev libxft-dev libfreetype6 libfreetype6-dev redis-server php-redis postfix sshpass zsh php-xdebug mysql-server-5.7 build-essential libsqlite3-dev ruby-dev ruby php-sqlite3 > /dev/null 2>&1
+    sudo apt-get install -y php"${php_install_version}"-common php"${php_install_version}"-dev php"${php_install_version}"-cli php"${php_install_version}"-fpm curl php"${php_install_version}"-curl php"${php_install_version}"-gd mcrypt php"${php_install_version}"-mysql php"${php_install_version}"-imap php"${php_install_version}"-mbstring php"${php_install_version}"-zip nginx php"${php_install_version}"-bcmath php"${php_install_version}"-xml chrpath libssl-dev libxft-dev libfreetype6 libfreetype6-dev redis-server php"${php_install_version}"-redis postfix sshpass zsh php"${php_install_version}"-xdebug mysql-server-5.7 build-essential libsqlite3-dev ruby-dev ruby php"${php_install_version}"-sqlite3 > /dev/null 2>&1
 
     echo_msg ${GREEN} "Removing apache"
-    sudo apt remove -y apache2 > /dev/null 2>&1
+    sudo apt-get remove -y apache2 > /dev/null 2>&1
 
     echo_msg ${GREEN} "Cleaning up after package install"
-    sudo apt autoremove -y > /dev/null 2>&1
+    sudo apt-get autoremove -y > /dev/null 2>&1
     sudo apt-add-repository ppa:brightbox/ruby-ng-experimental -y > /dev/null 2>&1
-    sudo apt update -y > /dev/null 2>&1
-    sudo sed -i '/memory_limit/c\memory_limit = -1' /etc/php/7.2/cli/php.ini
-    sudo sed -i '/max_execution_time/c\max_execution_time = 0' /etc/php/7.2/cli/php.ini
-    sudo cp ${home}/setup/config/xdebug.ini /etc/php/7.2/mods-available/withxdebug.ini
-    sudo cp ${home}/setup/config/noxdebug.ini /etc/php/7.2/mods-available/withoutxdebug.ini
+    sudo apt-get update -y > /dev/null 2>&1
+    sudo sed -i '/memory_limit/c\memory_limit = -1' /etc/php/"${php_install_version}"/cli/php.ini
+    sudo sed -i '/max_execution_time/c\max_execution_time = 0' /etc/php/"${php_install_version}"/cli/php.ini
+    sudo cp "${home}"/setup/config/xdebug.ini /etc/php/"${php_install_version}"/mods-available/withxdebug.ini
+    sudo cp "${home}"/setup/config/noxdebug.ini /etc/php/"${php_install_version}"/mods-available/withoutxdebug.ini
 
     echo_msg ${GREEN} "Configuring redis"
     sudo systemctl enable redis-server.service > /dev/null 2>&1
@@ -65,15 +84,17 @@ if [[ "$phppresent" = "" ]]; then
     sudo service redis-server restart > /dev/null 2>&1
 fi
 
-if [[ ! -e /etc/default/phantomjs ]]; then
-    echo_msg ${GREEN} "Installing phantomjs"
-    export PHANTOM_JS="phantomjs-2.1.1-linux-x86_64" > /dev/null 2>&1
-    wget https://github.com/Medium/phantomjs/releases/download/v2.1.1/${PHANTOM_JS}.tar.bz2 > /dev/null 2>&1
-    sudo tar xvjf ${PHANTOM_JS}.tar.bz2 > /dev/null 2>&1
-    sudo mv ${PHANTOM_JS} /usr/local/share > /dev/null 2>&1
-    sudo ln -sf /usr/local/share/${PHANTOM_JS}/bin/phantomjs /usr/local/bin > /dev/null 2>&1
-    sudo touch /etc/default/phantomjs > /dev/null 2>&1
-    echo "WEBDRIVER_PORT=4444" > sudo /etc/default/phantomjs
+if [[ "$phantomjs" = "yes" ]]; then
+  if [[ ! -e /etc/default/phantomjs ]]; then
+      echo_msg ${GREEN} "Installing phantomjs"
+      export PHANTOM_JS="phantomjs-2.1.1-linux-x86_64" > /dev/null 2>&1
+      wget https://github.com/Medium/phantomjs/releases/download/v2.1.1/${PHANTOM_JS}.tar.bz2 > /dev/null 2>&1
+      sudo tar xvjf ${PHANTOM_JS}.tar.bz2 > /dev/null 2>&1
+      sudo mv ${PHANTOM_JS} /usr/local/share > /dev/null 2>&1
+      sudo ln -sf /usr/local/share/${PHANTOM_JS}/bin/phantomjs /usr/local/bin > /dev/null 2>&1
+      sudo touch /etc/default/phantomjs > /dev/null 2>&1
+      echo "WEBDRIVER_PORT=4444" > sudo /etc/default/phantomjs
+  fi
 fi
 
 phpmailcatcher=`which mailcatcher`
@@ -83,11 +104,11 @@ if [[ "$phpmailcatcher" = "" ]]; then
     sudo gem install mailcatcher --no-ri --no-rdoc > /dev/null 2>&1
     echo "@reboot root $(which mailcatcher) --ip=0.0.0.0" >> sudo /etc/crontab
     sudo update-rc.d cron defaults > /dev/null 2>&1
-    sudo touch /etc/php/7.2/mods-available/mailcatcher.ini
-    echo "sendmail_path = /usr/bin/env $(which catchmail) -f 'www-data@localhost'" >> sudo /etc/php/7.2/mods-available/mailcatcher.ini
+    sudo touch /etc/php/"${php_install_version}"/mods-available/mailcatcher.ini
+    echo "sendmail_path = /usr/bin/env $(which catchmail) -f 'www-data@localhost'" >> sudo /etc/php/"${php_install_version}"/mods-available/mailcatcher.ini
     sudo phpenmod -v ALL -s ALL mailcatcher
     sudo cp ${home}/setup/mailcatcher.conf /etc/init/mailcatcher.conf > /dev/null 2>&1
-    sudo service php7.2-fpm restart > /dev/null 2>&1
+    sudo service php"${php_install_version}"-fpm restart > /dev/null 2>&1
     /usr/bin/env $(which mailcatcher) --ip=0.0.0.0 > /dev/null 2>&1
     echo '@reboot root $(which mailcatcher) --ip=0.0.0.0' >> sudo /etc/crontab > /dev/null 2>&1
     sudo update-rc.d cron defaults > /dev/null 2>&1
@@ -166,14 +187,16 @@ if [[ ! -e /etc/nginx/sites-available/${folder_name} ]]; then
     sudo sed -i "s/site_name/${folder_name}.local/g" /etc/nginx/sites-available/${folder_name}
     sudo sed -i "s/folder_path/${nginxpath}/g" /etc/nginx/sites-available/${folder_name}
     sudo sed -i "s/folder_name/${folder_name}/g" /etc/nginx/sites-available/${folder_name}
-    sudo sed -i "s/php_version/7.2/g" /etc/nginx/sites-available/${folder_name}
+    sudo sed -i "s/php_version/${php_version}/g" /etc/nginx/sites-available/${folder_name}
     sudo ln -s /etc/nginx/sites-available/${folder_name} /etc/nginx/sites-enabled/
     sudo rm -rf /etc/nginx/sites-available/default
     sudo rm /etc/nginx/sites-enabled/default
     service nginx restart > /dev/null 2>&1
 fi
 
-echo_msg ${GREEN} "Configuring google chromedriver"
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - > /dev/null 2>&1
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-sudo apt update -y && sudo apt install -y google-chrome-stable xvfb > /dev/null 2>&1
+if [[ "${use_chromedriver}" = 'yes' ]]; then
+    echo_msg ${GREEN} "Configuring google chromedriver"
+    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - > /dev/null 2>&1
+    sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+    sudo apt update -y && sudo apt install -y google-chrome-stable xvfb > /dev/null 2>&1
+fi
